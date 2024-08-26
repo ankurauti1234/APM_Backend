854cbb21c2a83b6a3ddc0bfa554eea6ecffbb6e5
Ankur Auti <ankurauti@gmail.com>
2024-08-26 16:07:10 +0530

    pathnames changed

--- config/mqtt.js
+++ config/mqtt.js
@@ -1,10 +1,18 @@
 const path = require("path");
 
+const certPath = path.join(__dirname, "./config/certs/test.cert.pem.crt");
+const keyPath = path.join(__dirname, "./config/certs/test.private.pem.key");
+const caPath = path.join(__dirname, "./config/certs/root-CA.crt");
+
+console.log("Certificate Path:", certPath);
+console.log("Key Path:", keyPath);
+console.log("CA Path:", caPath);
+
 module.exports = {
   endpoint: "a3uoz4wfsx2nz3-ats.iot.ap-south-1.amazonaws.com",
-  certPath: path.join(__dirname, "./certs/test.cert.pem.crt"),
-  keyPath: path.join(__dirname, "./certs/test.private.pem.key"),
-  caPath: path.join(__dirname, "./certs/root-ca.crt"),
+  certPath,
+  keyPath,
+  caPath,
   clientId: "mqtt-client-" + Math.random().toString(16).substr(2, 8),
   topicsToSubscribe: ["apm/device/data", "apm/logo"],
 };
